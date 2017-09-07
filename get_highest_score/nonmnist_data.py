f6d324bb2b446bee3f5e8cf9333f3e4dc40ec959
scott huang <scotthuang1989@163.com>
2017-09-07 09:40:36 +0800

    get_highest_core: paritial comment

--- get_highest_score/nonmnist_data.py
+++ get_highest_score/nonmnist_data.py
@@ -5,6 +5,7 @@ import tarfile
 # import matplotlib.pyplot as plt
 import numpy as np
 from scipy import ndimage
+import tensorflow as tf
 
 
 url = 'https://commondatastorage.googleapis.com/books1000/'
@@ -71,12 +72,14 @@ def maybe_extract(filename, force=False):
 
 # build ndarray to store image and label
 
-def load_letter(folder,label,image_size=28):
+def load_letter(folder,label,image_size=28,sample_num=-1):
   """Load the data for a single letter label."""
   image_files = os.listdir(folder)
   dataset = np.ndarray(shape=(len(image_files), image_size, image_size),
                          dtype=np.float32)
   num_images = 0
+  if sample_num == -1:
+      sample_num = len(image_files)
   for image in image_files:
     image_file = os.path.join(folder, image)
     try:
@@ -85,6 +88,8 @@ def load_letter(folder,label,image_size=28):
         raise Exception('Unexpected image shape: %s' % str(image_data.shape))
       dataset[num_images, :, :] = image_data
       num_images = num_images + 1
+      if num_images >= sample_num:
+          break
     except IOError as e:
       print('Could not read:', image_file, ':', e, '- it\'s ok, skipping.')
 
@@ -92,6 +97,77 @@ def load_letter(folder,label,image_size=28):
   data_label = np.ndarray(shape=(num_images), dtype=np.int8)
   data_label.fill(label)
   return dataset,data_label
+
+
+def _int64_feature(value):
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _bytes_feature(value):
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def convert_to(data_set, data_set_label, name):
+  """
+  args:
+  data_set: a ndarray with following shape: [data_set_size,height,width]
+  data_set_lable: a ndarray with following shape: [data_set_size]
+  name: the name of generated tfrecoords file: name.tfrecoords
+  return:
+    none
+  """
+  if data_set.shape[0] != data_set_label.shape[0]:
+    raise ValueError('Images size %d does not match label size %d.' %
+                     (data_set.shape[0], data_set_label.shape[0]))
+
+  num_examples = data_set.shape[0]
+  rows = data_set.shape[1]
+  cols = data_set.shape[2]
+
+  filename = name + '.tfrecords'
+  print('Writing', filename)
+  writer = tf.python_io.TFRecordWriter(filename)
+  for index in range(num_examples):
+    image_raw = data_set[index].tostring()
+    example = tf.train.Example(features=tf.train.Features(feature={
+        'height': _int64_feature(rows),
+        'width': _int64_feature(cols),
+        'label': _int64_feature(data_set_label[index]),
+        'image_raw': _bytes_feature(image_raw)}))
+    writer.write(example.SerializeToString())
+  writer.close()
+
+
+def convert_back(file_name)
+    """
+    target:
+    convert a tfrecods without difining a graph. for test purpose
+    args:
+        file_name: file name of tfrecods file.abs
+    return:
+        (reconstructed_image,reconstructed_label)
+    """
+    constructed_data = None
+    constructed_label = None
+    record_iterator = tf.python_io.tf_record_iterator(path=file_name)
+
+    for record in record_iterator:
+        example = tf.train.Example()
+        example.ParseFromString(record)
+        height = int(example.features['height'].int64_list.value[0])
+        width = int(example.features['width'].int64_list.value[0])
+        label = int(example.features['label'].int64_list.value[0])
+        image_raw = example.features['image_raw'].bytes_list.value[0]
+
+        img_1d = np.fromstring(image_raw,dtype=np.uint8)
+        img = img_1d.reshape((height,width))
+        if constructed_data == None:
+            constructed_data = img
+            constructed_label = np.array(label)
+        else:
+            np.concatenate([constructed_data,img])
+            np.append(constructed_label,label)
+    return constructed_data,constructed_label
 
 
 def make_tfrecords():
@@ -109,4 +185,5 @@ def make_tfrecords():
         # concatenate the whole list and shuffle it.
     train_dataset = np.concatenate([ train_data_list[i][0] for i in range(len(train_data_list))])
     train_label = np.concatenate([train_data_list[i][1] for i in range(len(train_data_list))])
+    ## todo: shuffer the whole list.
     return train_dataset,train_label
